4bd75fce84efd1670d826bc595f0446e692748cc
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-05-01 13:26:29 -0700

    Save and deleted buttons added

--- med_cabinet_react/src/components/recCard.js
+++ med_cabinet_react/src/components/recCard.js
@@ -5,8 +5,12 @@ import WeedCardStyle from '../styles/weedCard'
 // const dummyData = [{ "id": 6, "Strain": "24K-Gold", "Type": "hybrid", "Rating": "4.6", "Effects": "Happy,Relaxed,Euphoric,Uplifted,Talkative", "Flavor": "Citrus,Earthy,Orange", "Description": "Also known as Kosher Tangie, 24k Gold is a 60% indica-dominant hybrid that combines the legendary LA strain Kosher Kush with champion sativa Tangie to create something quite unique. Growing tall in its vegetative cycle and very stretchy in flower, this one will need an experienced hand when grown indoors. Most phenotypes will exhibit a sweet orange aroma from the Tangie along with the dark coloration of the Kosher Kush, and will offer a strong citrus flavor when smoked or vaped. THC levels range from 18% to 24%; definitely not for novice users!" }]
 
 function WeedCard( props ) {
-    const { strain } = props;
+    const { strain, saveStrain } = props;
     console.log("Inside WeedCard", strain);
+
+    function onSave (event){
+        saveStrain(strain);
+    }
 
     return (
         <div className='cardWrapper'>
@@ -19,10 +23,8 @@ function WeedCard( props ) {
                     <span className='descWrapper'>
                         <p>{strain.Description}</p>
                     </span>
+                    <button className={strain.Strain} onClick={onSave}>Save me!</button>
                 </WeedCardStyle>
-            {/* {props.strains.map(strain => {
-
-            })} Commented this out for*/}
         </div>
     )
 
--- med_cabinet_react/src/components/saves.jsx
+++ med_cabinet_react/src/components/saves.jsx
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react'
 
-function SavedStrains (saves){
+export default function SavedStrains (saves){
     const list=Object.values(saves)
     console.log(list)
 
-    return(
-        <div className='saves-wrapper'>
-            {list.map(li =>{
-                return(<h3>{li.Strain}</h3>)
-            })}
-        </div>
-    )
-}
 
-export default SavedStrains
+        return(
+            <div className='saves-wrapper'>
+                <ul>
+                {list.map(li =>{
+                    return(<h3>{li.Strain}</h3>)
+                })}
+                </ul>
+            </div>
+        )
+    }
+
+
+
